b129af772dd3745f9c13b60aefd8f47b1597e23b
Sven Ruppert <sven.ruppert@gmail.com>
2019-06-24 21:04:36 +0200

    updated maven combinations

---
+++ _data/zulu@1.8.112_maven_3.0.5/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+function docker_tag_exists() {
+    EXISTS=$(curl -s  https://hub.docker.com/v2/repositories/$1/tags/?page_size=10000 | jq -r "[.results | .[] | .name == \"$2\"] | any")
+    test $EXISTS = true
+}
+
+if docker_tag_exists svenruppert/maven-3.0.5-zulu 1.8.112; then
+    echo skip building, image already existing - svenruppert/maven-3.0.5-zulu 1.8.112
+else
+    echo start building the images
+    docker build -t svenruppert/maven-3.0.5-zulu .
+    docker tag svenruppert/maven-3.0.5-zulu:latest svenruppert/maven-3.0.5-zulu:1.8.112
+    docker push svenruppert/maven-3.0.5-zulu:1.8.112
+fi
