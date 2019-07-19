cfaab8cacb25028c29a3c364c0f895b0fc3088d7
Sven Ruppert <sven.ruppert@gmail.com>
2019-07-19 14:18:01 +0200

    updated jdk versions

---
+++ _data/openjdk@1.12.0-2/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+function docker_tag_exists() {
+    EXISTS=$(curl -s  https://hub.docker.com/v2/repositories/$1/tags/?page_size=10000 | jq -r "[.results | .[] | .name == \"$2\"] | any")
+    test $EXISTS = true
+}
+
+if docker_tag_exists svenruppert/openjdk 1.12.0-2; then
+    echo skip building, image already existing - svenruppert/openjdk:1.12.0-2
+else
+    echo start building the images
+    docker build -t svenruppert/openjdk .
+
+    docker tag svenruppert/openjdk:latest svenruppert/openjdk:1.12.0-2
+    docker push svenruppert/openjdk:1.12.0-2
+fi
